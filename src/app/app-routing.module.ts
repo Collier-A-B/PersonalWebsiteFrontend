c6909e313a825ab17523f2ff0637f2695a8abc68
Collier-A-B <benjaminac46@gmail.com>
2025-07-14 15:42:50 -0400

    routing fix attempt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ResumeComponent } from './resume/resume.component';
-import { RouteNotFoundComponentComponent } from './route-not-found-component/route-not-found-component.component';
 
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: '', redirectTo: '/profile', pathMatch: 'full' },
-  { path: '**', component: RouteNotFoundComponentComponent}
 ];
 
 @NgModule({
